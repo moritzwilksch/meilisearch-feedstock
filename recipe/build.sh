d3ac0d0f8a55c3b9288e77fe3695871759933e20
Moritz Wilksch <moritz.wilksch@quantco.com>
2025-04-24 16:44:45 +0200

    novendor

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,7 @@ cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 export RUST_BACKTRACE=1
 export MEILI_NO_VERGEN=0
-export GIT2_SYS_USE_PKG_CONFIG=1
-export PKG_CONFIG_ALLOW_CROSS=1
-export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig
+export LIBGIT2_NO_VENDOR=1
 cargo build --release --locked
 
 
