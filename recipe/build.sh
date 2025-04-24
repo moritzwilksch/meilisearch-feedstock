b4f41bd56a51796afa98f277dd7e429206fae59d
Moritz Wilksch <moritz.wilksch@quantco.com>
2025-04-24 15:34:19 +0200

    env?

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@ cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 export RUST_BACKTRACE=1
 export MEILI_NO_VERGEN=0
+export GIT2_SYS_USE_PKG_CONFIG=1
+export PKG_CONFIG_ALLOW_CROSS=1
+export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig
 cargo build --release --locked
 
 
