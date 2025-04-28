cb73d5a36720fbd593ce3fe6451ee783a696f0f8
Uwe L. Korn <uwe.korn@quantco.com>
2025-04-28 15:15:38 +0200

    Set DYLD_FALLBACK_LIBRARY_PATH

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,15 @@
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+#!/bin/bash
+
+set -euxo pipefail
 
 export RUST_BACKTRACE=1
 export MEILI_NO_VERGEN=0
 export LIBGIT2_NO_VENDOR=1
+export DYLD_FALLBACK_LIBRARY_PATH="${BUILD_PREFIX}/lib"
 cargo build --release --locked
-
 
 mkdir -p $PREFIX/bin/
 mv ./target/*/release/meilisearch $PREFIX/bin/meilisearch
 mv ./target/*/release/meilitool $PREFIX/bin/meilitool
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
