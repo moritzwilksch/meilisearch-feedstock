8f4dc3f866d5cc8e3dc9b1f1ca2cd03ad1d6515a
Uwe L. Korn <uwe.korn@quantco.com>
2025-05-05 11:33:39 +0200

    Use alternative libssh2 name

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,5 +1,7 @@
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+COPY %LIBRARY_PREFIX%\lib\ssh2.lib %LIBRARY_PREFIX%\lib\libssh2.lib
 cargo build --release --locked
+RM %LIBRARY_PREFIX%\lib\libssh2.lib
 
 MKDIR %LIBRARY_PREFIX%\bin
 
